a06a3e2fae33b401a833f88bda08b08ac3c9af53
Jazzmin Vangeel <jazzvangeel@gmail.com>
2015-10-29 10:07:49 -0400

    wednesday night changes

--- header.php
+++ header.php
@@ -8,6 +8,8 @@
   <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 
   <?php // Load our CSS ?>
+  <link href='https://fonts.googleapis.com/css?family=Work+Sans:400,300,200,500' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
   <link rel="stylesheet" type="text/css" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
 
   <?php wp_head(); ?>

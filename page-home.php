0a1fa1ae80fcaae562a0321912d554e83c9f2d8b
Jazzmin Vangeel <jazzvangeel@gmail.com>
2015-11-01 13:02:52 -0500

    tidying up

--- page-home.php
+++ page-home.php
@@ -7,18 +7,34 @@
 	</div>
 </div>
 
-<div class="owners">
-	<div class="ownerOne"></div>
-	<div class="ownerTwo"></div>
-	<h3>Meet The Owners!</h3>
-	<p>Tendrils of gossamer clouds Cambrian explosion. Inconspicuous motes of rock and gas, vanquish the impossible birth emerged into consciousness, hydrogen atoms billions upon billions from which we spring rich in heavy atoms the sky calls to us.</p>
-	<h3>&amp; the rest of the Team!</h3>
-	<p>Emerged into consciousness tingling of the spine concept of the number one intelligent beings dream of the mind's eye at the edge of forever vanquish the impossible sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
-		<div class="team">
-			<div class="teamOne"></div>
-			<div class="teamTwo"></div>
-			<div class="teamThree"></div>
+<div class="panelOne">
+	<div class="owners">
+		<div class="owner">
+			<div class="ownerOne"></div>
+			<div class="ownerTwo"></div>
 		</div>
+		<div class="ownerText">
+			<h3 class="owner" >Meet The Owners!</h3>
+			<p class="owner" >Tendrils of gossamer clouds Cambrian explosion. Inconspicuous motes of rock and gas, vanquish the impossible birth emerged into consciousness, hydrogen atoms billions upon billions from which we spring rich in heavy atoms the sky calls to us.</p>
+		</div>
+	</div>
+	<div class="team">
+		<div class="teamText">
+			<h3 class="team">&amp; the rest of the Team!</h3>
+			<p class="team">Emerged into consciousness tingling of the spine concept of the number one intelligent beings dream of the mind's eye at the edge of forever vanquish the impossible sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
+		</div>
+			<div class="teamPhoto">
+				<div class="teamOne"></div>
+				<div class="teamTwo"></div>
+				<div class="teamThree"></div>
+			</div>
+	</div>
 </div>
 
+<div class="bannerOne">
+	<div class="bannerImageOne">
+		<h4>Trillion great turbulent clouds, shores of the cosmic ocean, circumnavigated brain is the seed of intelligence and billions upon billions upon billions upon billions upon billions upon billions upon billions.</h4>
+	</div>
+
+</div>
 <?php get_footer(); ?>
